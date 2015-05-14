878695ce91202fbd20a8716d598b32e6343a4b20
phuchit <ripzery@gmail.com>
2015-05-14 14:16:57 +0700

    add angularjs power add ui-router

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
-var app =
+var myApp = angular.module('myApp', ['ui.router']);
+
+// define route and controller for each view
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/index");
+    $stateProvider
+        .state('index', {
+            url: '/index',
+            views: {
+                "navbar":{
+                    templateUrl: "header.html",
+                    controller: 'homeController'
+                },
+                "content":{
+                    templateUrl: "home.html"
+                }
+            }
+
+        })
+        .state('admin', {
+            url: '/admin',
+            views: {
+                "navbar":{
+                    templateUrl: "admin_header.html",
+                    controller: 'adminController'
+                },
+                "content":{
+                    templateUrl: "admin.html"
+                }
+            }
+        });
+});
+
+myApp.controller('homeController',function($scope,$state){
+    $scope.login = function(){
+        console.log("/login");
+        $state.go('admin');
+    };
+
+});
+
+myApp.controller('adminController',function($scope,$state){
+    $scope.logout = function(){
+        console.log("/logout");
+        $state.go('index');
+    };
+});
